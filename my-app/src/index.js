ef0006628c7bcd544774e9ad0a5a592bf285fd2b
calvin.huynh <calvin.huynh@objectbrains.com>
2019-05-29 20:23:02 -0700

    style

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import "./styles.css";
+
+var name = 'Calvin'
+var element = <span id>Click the buttons bellow to roll stats for tabletop RPG's (i.e D&D/Pathfinder)</span>
+var rollStatsButtonStandard = <button type="button" onclick="rollStats()">Standard</button>
+var rollStatsButtonClassic = <button type="button" onclick="rollStats()">Classic</button>
+var rollStatsButtonHeroic = <button type="button" onclick="rollStats()">Heroic</button>
+var statBlock = 0
+
+function StatBlock(){
+  return <div>
+    <table>
+      <tr>
+        <td>
+          STR
+        </td>
+        <td>
+          {statBlock}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          DEX
+        </td>
+        <td>
+          {statBlock}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          CON
+        </td>
+        <td>
+          {statBlock}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          INT
+        </td>
+        <td>
+          {statBlock}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          WIS
+        </td>
+        <td>
+          {statBlock}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          CHA
+        </td>
+        <td>
+          {statBlock}
+        </td>
+      </tr>
+    </table>
+  </div>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h2>{element}</h2>
+      <div>
+        {rollStatsButtonStandard}{rollStatsButtonClassic}{rollStatsButtonHeroic}
+      </div>
+      <StatBlock/>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
